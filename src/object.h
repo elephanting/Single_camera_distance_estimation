036138ac780161b36e798bfc7b905b5a9183a191
elephanting <elephanting5566@gmail.com>
2019-12-09 01:48:12 +0800

    update

---
+++ src/object.h
@@ -0,0 +1,12 @@
+class Object
+{
+public:
+    Object(float, float);
+    void estimate_dis();
+    void estimate_h();
+    ~Object();
+private:
+    float topleft;
+    float bottomright;
+    float distance;
+}
